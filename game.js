dd606478b8ddac60a05e19fd3ad20d86a20b175c
Jonahkok123 <jk.173716@student.shore.nsw.edu.au>
2026-06-01 16:43:57 +1000

    Update game.js

--- game.js
+++ game.js
@@ -3,16 +3,39 @@ const ctx = canvas.getContext("2d");
 
 let player = {
     x: 100,
-    y: 200
+    y: 200,
+    speed: 3
 };
 
-function loop() {
+let keys = {};
+
+// input
+document.addEventListener("keydown", (e) => {
+    keys[e.key.toLowerCase()] = true;
+});
+
+document.addEventListener("keyup", (e) => {
+    keys[e.key.toLowerCase()] = false;
+});
+
+function update() {
+    if (keys["w"]) player.y -= player.speed;
+    if (keys["s"]) player.y += player.speed;
+    if (keys["a"]) player.x -= player.speed;
+    if (keys["d"]) player.x += player.speed;
+}
+
+function draw() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.fillStyle = "cyan";
     ctx.fillRect(player.x, player.y, 40, 40);
+}
 
+function loop() {
+    update();
+    draw();
     requestAnimationFrame(loop);
 }
 
