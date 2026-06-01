7792e938cb06eff95ed33013d2682192eb5df8c2
Jonahkok123 <jk.173716@student.shore.nsw.edu.au>
2026-06-01 16:37:45 +1000

    Update game.js

--- game.js
+++ game.js
@@ -3,10 +3,13 @@ const ctx = canvas.getContext("2d");
 
 let x = 100;
 
+// GAME LOOP
 function loop() {
+    // clear screen
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    // draw moving square
     ctx.fillStyle = "red";
     ctx.fillRect(x, 200, 50, 50);
 
@@ -15,4 +18,5 @@ function loop() {
     requestAnimationFrame(loop);
 }
 
+// start game
 loop();
