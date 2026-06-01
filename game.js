d0f7fc430a1e5ad51bf01a65c19f4ee2e1bd9fc1
Jonahkok123 <jk.173716@student.shore.nsw.edu.au>
2026-06-01 16:39:48 +1000

    Update game.js

--- game.js
+++ game.js
@@ -1,22 +1,32 @@
 const canvas = document.getElementById("c");
 const ctx = canvas.getContext("2d");
 
-let x = 100;
+let player = {
+    x: 100,
+    y: 200,
+    speed: 3
+};
 
-// GAME LOOP
-function loop() {
-    // clear screen
+let keys = {};
+
+document.addEventListener("keydown", e => keys[e.key.toLowerCase()] = true);
+document.addEventListener("keyup", e => keys[e.key.toLowerCase()] = false);
+
+function update() {
+    if (keys["w"]) player.y -= player.speed;
+    if (keys["s"]) player.y += player.speed;
+    if (keys["a"]) player.x -= player.speed;
+    if (keys["d"]) player.x += player.speed;
+}
+
+function draw() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // draw moving square
-    ctx.fillStyle = "red";
-    ctx.fillRect(x, 200, 50, 50);
-
-    x += 2;
-
-    requestAnimationFrame(loop);
+    ctx.fillStyle = "cyan";
+    ctx.fillRect(player.x, player.y, 40, 40);
 }
 
-// start game
-loop();
+function loop() {
+    update();
+    draw();
