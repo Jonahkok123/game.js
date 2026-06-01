776ff67ebd9870c3a4139b815b5400711e59afac
Jonahkok123 <jk.173716@student.shore.nsw.edu.au>
2026-06-01 16:46:23 +1000

    Update game.js

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ let player = {
 };
 
 let keys = {};
+let bullets = [];
 
 // input
 document.addEventListener("keydown", (e) => {
@@ -18,19 +19,42 @@ document.addEventListener("keyup", (e) => {
     keys[e.key.toLowerCase()] = false;
 });
 
+// click to shoot
+canvas.addEventListener("click", () => {
+    bullets.push({
+        x: player.x,
+        y: player.y,
+        speed: 5
+    });
+});
+
 function update() {
+    // movement
     if (keys["w"]) player.y -= player.speed;
     if (keys["s"]) player.y += player.speed;
     if (keys["a"]) player.x -= player.speed;
     if (keys["d"]) player.x += player.speed;
+
+    // move bullets
+    bullets.forEach(b => {
+        b.x += b.speed;
+    });
 }
 
 function draw() {
+    // background
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    // player
     ctx.fillStyle = "cyan";
     ctx.fillRect(player.x, player.y, 40, 40);
+
+    // bullets
+    ctx.fillStyle = "orange";
+    bullets.forEach(b => {
+        ctx.fillRect(b.x, b.y, 10, 5);
+    });
 }
 
 function loop() {
